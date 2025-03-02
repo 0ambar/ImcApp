06ad8c9072b21eb5ec2e87faaeda0696a0117daf
0ambar <abrahamr677@gmail.com>
2025-03-01 22:21:22 -0600

    Initial commit

---
+++ app/src/main/java/com/example/helloandroid/ShowNameActivity.kt
@@ -0,0 +1,26 @@
+package com.example.helloandroid
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ShowNameActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_show_name)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val tvShowName = findViewById<TextView>(R.id.tvShowName)
+        // Assert the value has a String type or empty string if not
+        val name: String = intent.extras?.getString("EXTRA_NAME").orEmpty()
+        tvShowName.text = "Hola $name"
+    }
+}
---
+++ app/src/main/java/com/example/helloandroid/imc_calculator/ImcCalculatorActivity.kt
@@ -0,0 +1,93 @@
+package com.example.helloandroid.imc_calculator
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.helloandroid.R
+import com.google.android.material.slider.RangeSlider
+
+class ImcCalculatorActivity : AppCompatActivity() {
+
+    private var isMaleSelected: Boolean = true
+    private var isFemaleSelected: Boolean = false
+
+    private lateinit var viewMale: CardView
+    private lateinit var viewFemale: CardView
+    private lateinit var tvHeight: TextView
+    private lateinit var rsHeight: RangeSlider
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_imc_calculator)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        initComponent()
+        initListeners()
+        initUI()
+    }
+
+
+    private fun initComponent() {
+        viewMale = findViewById(R.id.viewMale)
+        viewFemale = findViewById(R.id.viewFemale)
+        tvHeight = findViewById(R.id.tvHeight)
+
+        rsHeight = findViewById(R.id.rsHeight)
+    }
+
+    private fun initListeners() {
+        viewMale.setOnClickListener{
+            changeGender("male")
+            setGenderColor()
+        }
+
+        viewFemale.setOnClickListener{
+            changeGender("female")
+            setGenderColor()
+        }
+
+//        srHeight.addOnChangeListener()
+
+    }
+
+    private fun changeGender(gender: String) {
+        if(gender == "male") {
+            isMaleSelected = true
+            isFemaleSelected = false
+        } else {
+            isMaleSelected = false
+            isFemaleSelected = true
+        }
+
+    }
+
+    private fun setGenderColor() {
+        viewMale.setCardBackgroundColor(getBackgroundColor(isMaleSelected))
+        viewFemale.setCardBackgroundColor(getBackgroundColor(isFemaleSelected))
+    }
+
+    private fun getBackgroundColor(isSelectedComponent: Boolean): Int {
+        val colorReference = if(isSelectedComponent) {
+            R.color.background_component_selected
+        } else {
+            R.color.background_component
+        }
+
+        return ContextCompat.getColor(this, colorReference)
+    }
+
+    private fun initUI() {
+        setGenderColor()
+    }
+}
